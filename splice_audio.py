8ba0777f5ee12b92e9281e2cacb96c00193d6e51
FarisHijazi <theefaris@gmail.com>
2020-06-16 11:47:11 +0300

    splice_audio: refactor: moved functions, and added pydub functions with tqdm

--- splice_audio.py
+++ splice_audio.py
@@ -29,8 +29,220 @@ from pydub.utils import db_to_float
 from tqdm import tqdm
 
 
+# === pydub function ===
 
-def _force_short_chunks(audio_chunks, max_len=15000, min_silence_len=100, silence_thresh=-50.0, scaledown=0.9) -> list:
+def detect_silence(audio_segment, min_silence_len=1000, silence_thresh=-16, seek_step=1, disable_tqdm=True):
+    seg_len = len(audio_segment)
+
+    # you can't have a silent portion of a sound that is longer than the sound
+    if seg_len < min_silence_len:
+        return []
+
+    # convert silence threshold to a float value (so we can compare it to rms)
+    silence_thresh = db_to_float(silence_thresh) * audio_segment.max_possible_amplitude
+
+    # find silence and add start and end indicies to the to_cut list
+    silence_starts = []
+
+    # check successive (1 sec by default) chunk of sound for silence
+    # try a chunk at every "seek step" (or every chunk for a seek step == 1)
+    last_slice_start = seg_len - min_silence_len
+    slice_starts = range(0, last_slice_start + 1, seek_step)
+
+    # guarantee last_slice_start is included in the range
+    # to make sure the last portion of the audio is searched
+    if last_slice_start % seek_step:
+        slice_starts = itertools.chain(slice_starts, [last_slice_start])
+
+    for i in tqdm(slice_starts, desc='slicing silences', disable=disable_tqdm):
+        audio_slice = audio_segment[i:i + min_silence_len]
+        if audio_slice.rms <= silence_thresh:
+            silence_starts.append(i)
+
+    # short circuit when there is no silence
+    if not silence_starts:
+        return []
+
+    # combine the silence we detected into ranges (start ms - end ms)
+    silent_ranges = []
+
+    prev_i = silence_starts.pop(0)
+    current_range_start = prev_i
+
+    for silence_start_i in silence_starts:
+        continuous = (silence_start_i == prev_i + seek_step)
+
+        # sometimes two small blips are enough for one particular slice to be
+        # non-silent, despite the silence all running together. Just combine
+        # the two overlapping silent ranges.
+        silence_has_gap = silence_start_i > (prev_i + min_silence_len)
+
+        if not continuous and silence_has_gap:
+            silent_ranges.append([current_range_start,
+                                  prev_i + min_silence_len])
+            current_range_start = silence_start_i
+        prev_i = silence_start_i
+
+    silent_ranges.append([current_range_start,
+                          prev_i + min_silence_len])
+
+    return silent_ranges
+
+
+def detect_nonsilent(audio_segment, min_silence_len=1000, silence_thresh=-16, seek_step=1, disable_tqdm=True):
+    silent_ranges = detect_silence(audio_segment, min_silence_len, silence_thresh, seek_step, disable_tqdm=disable_tqdm)
+    len_seg = len(audio_segment)
+    # if there is no silence, the whole thing is nonsilent
+    if not silent_ranges:
+        return [[0, len_seg]]
+
+    # short circuit when the whole audio segment is silent
+    if silent_ranges[0][0] == 0 and silent_ranges[0][1] == len_seg:
+        return []
+
+    prev_end_i = 0
+    nonsilent_ranges = []
+    for start_i, end_i in silent_ranges:
+        nonsilent_ranges.append([prev_end_i, start_i])
+        prev_end_i = end_i
+
+    if end_i != len_seg:
+        nonsilent_ranges.append([prev_end_i, len_seg])
+
+    if nonsilent_ranges[0] == [0, 0]:
+        nonsilent_ranges.pop(0)
+
+    return nonsilent_ranges
+
+
+def split_on_silence(audio_segment, min_silence_len=1000, silence_thresh=-16, keep_silence=100,
+                     seek_step=1, disable_tqdm=True):
+    """
+    audio_segment - original pydub.AudioSegment() object
+
+    min_silence_len - (in ms) minimum length of a silence to be used for
+        a split. default: 1000ms
+
+    silence_thresh - (in dBFS) anything quieter than this will be
+        considered silence. default: -16dBFS
+
+    keep_silence - (in ms or True/False) leave some silence at the beginning
+        and end of the chunks. Keeps the sound from sounding like it
+        is abruptly cut off.
+        When the length of the silence is less than the keep_silence duration
+        it is split evenly between the preceding and following non-silent
+        segments.
+        If True is specified, all the silence is kept, if False none is kept.
+        default: 100ms
+    """
+
+    # from the itertools documentation
+    def pairwise(iterable):
+        "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+    if isinstance(keep_silence, bool):
+        keep_silence = len(audio_segment) if keep_silence else 0
+
+    output_ranges = [
+        [start - keep_silence, end + keep_silence]
+        for (start, end)
+        in detect_nonsilent(audio_segment, min_silence_len, silence_thresh, seek_step, disable_tqdm=disable_tqdm)
+    ]
+
+    for range_i, range_ii in pairwise(output_ranges):
+        last_end = range_i[1]
+        next_start = range_ii[0]
+        if next_start < last_end:
+            range_i[1] = (last_end + next_start) // 2
+            range_ii[0] = range_i[1]
+
+    return [
+        audio_segment[max(start, 0): min(end, len(audio_segment))]
+        for start, end in output_ranges
+    ]
+
+
+def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
+    '''
+    sound is a pydub.AudioSegment
+    silence_threshold in dB
+    chunk_size in ms
+    iterate over chunks until you find the first one with sound
+    '''
+    trim_ms = 0  # ms
+    assert chunk_size > 0  # to avoid infinite loop
+    while sound[trim_ms:trim_ms + chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+        trim_ms += chunk_size
+
+    return trim_ms
+
+
+# === silence functions ===
+
+def remove_leading_and_trailing_silences(chunk, silence_thresh=-16, keep_silence=50, pbar=None):
+    # removing leading and trailing silences
+    start_trim = detect_leading_silence(chunk, silence_threshold=silence_thresh)
+    end_trim = len(chunk) - detect_leading_silence(chunk.reverse(), silence_threshold=silence_thresh)
+
+    chunk = chunk[max(0, start_trim - keep_silence): min(len(chunk), end_trim + keep_silence)]
+    if pbar is not None:
+        pbar.set_description(f"striping silences in range ({start_trim}, {end_trim})")
+    # remove silences
+    return chunk
+
+
+def _remove_silences(sound: AudioSegment, min_silence_len=500, silence_thresh=None):
+    # https://stackoverflow.com/a/60212565
+    non_sil_times = detect_nonsilent(
+        sound,
+        min_silence_len=min_silence_len,
+        silence_thresh=silence_thresh or sound.dBFS * 1.5
+    )
+    if non_sil_times:
+        sound = sum(sound[start:end] for start, end in non_sil_times)
+
+    return sound
+
+
+# === helpers ===
+
+def _check_samplerate(args):
+    # check samplerate
+    out = subprocess.Popen(['ffmpeg', '-i', args.input.as_posix()],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    stdout = stdout.decode('utf8')
+    stderr = stderr.decode('utf8') if stderr is not None else None
+
+    lines = stdout.split('\n')
+    segments = [line for line in lines if " Hz, " in line][0].split(', ')
+    number = [seg for seg in segments if " Hz" in seg][0].split(' ')[0]
+
+    samplerate = int(number)
+    if samplerate > 16000 and \
+            (args.force or input(f'Samplerate too high, create 16KHz version? [(Y)es/no]').lower() in ['n', 'no']):
+        input16khz = args.input.as_posix() + '.16KHz' + args.input.suffix
+        out = subprocess.Popen(['ffmpeg', '-i', args.input.as_posix(), '-ar', '16000', input16khz],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+        stdout, stderr = out.communicate()
+        stdout = stdout.decode('utf8')
+        stderr = stderr.decode('utf8') if stderr is not None else None
+
+        print(stdout)
+        if stderr or 'unable' in stdout:
+            print(f"Couldn't convert {args.input}")
+            raise Exception(stderr)
+        else:
+            args.input = Path(input16khz)
+            assert args.input.exists(), f'{args.input} does not exist! Failed to create 16Khz version'
+
+
+def _force_short_chunks(audio_chunks, max_len=15000, min_silence_len=500, silence_thresh=-16, scaledown=0.9) -> list:
     # ensure that all chunks are less than the max len:
     min_silence_len, silence_thresh = int(round(min_silence_len * scaledown)), silence_thresh * scaledown
     short_chunks = []
@@ -118,10 +330,10 @@ def _merge_short_segments_to_range(audio_chunks, min_len, max_len):
             merged_chunks.append(sum(subchunks))
     else:
         pbar.close()
-    lens = list(map(len, merged_chunks))
-    print(f'Lengths of merged_chunks: {lens}')
     return merged_chunks
 
+
+# === core functions ===
 
 def splice_using_silences(input: Path, out: Path, min_silence_len=500, silence_thresh=-16, out_fmt='flac', min_len=7000,
                           max_len=15000, **kwargs):
@@ -222,33 +434,6 @@ def splice_using_subtitles(input: Path, out: Path, subtitles: Path, min_silence_
             fmt_out_file = out_dir.joinpath(f"{out_parent_dir}-{filename}-{i:04d}.{out_fmt}")
             chunk.export(fmt_out_file, format=out_fmt)
     print(f'Segments have covered {(length / len(audio)) * 100:.2f}%')
-
-
-def remove_leading_and_trailing_silences(chunk, silence_thresh=-16, keep_silence=50, pbar=None):
-    # removing leading and trailing silences
-    start_trim = detect_leading_silence(chunk, silence_threshold=silence_thresh)
-    end_trim = len(chunk) - detect_leading_silence(chunk.reverse(), silence_threshold=silence_thresh)
-
-    chunk = chunk[max(0, start_trim - keep_silence): min(len(chunk), end_trim + keep_silence)]
-    if pbar is not None:
-        pbar.set_description(f"striping silences in range ({start_trim}, {end_trim})")
-    # remove silences
-    return chunk
-
-
-def _remove_silences(sound: AudioSegment, min_silence_len=500, silence_thresh=None):
-    # https://stackoverflow.com/a/60212565
-    non_sil_times = detect_nonsilent(
-        sound,
-        min_silence_len=min_silence_len,
-        silence_thresh=silence_thresh or sound.dBFS * 1.5
-    )
-    if non_sil_times:
-        sound = sum(sound[start:end] for start, end in non_sil_times)
-
-    return sound
-
-
 
 
 if __name__ == "__main__":
